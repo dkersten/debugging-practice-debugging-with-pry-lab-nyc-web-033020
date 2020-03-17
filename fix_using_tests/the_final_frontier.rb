1179e5930876229e5537744c7424fdaadde8714d
Daniel Kersten <dkersten1994@gmail.com>
2020-03-17 14:16:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -14,7 +14,8 @@ def crew
 end
 
 def greet_crew(crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  greeting_array = []
+  crew.each {|crew_member| greeting_array << "Hello #{crew_member}."}
 end
 
 def engage
